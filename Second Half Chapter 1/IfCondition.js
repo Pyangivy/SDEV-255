6f4c93a93781b47e3aedb59c8df4697f9d4c56b5
Pyangivy <130029014+Pyangivy@users.noreply.github.com>
2023-11-13 13:36:49 -0500

    chapter 1 second half

---
+++ Second Half Chapter 1/IfCondition.js
@@ -0,0 +1,9 @@
+// condition followed by a statement
+if (condition) statement; // statement executed if condition is true
+// condition followed by a blopck
+if (condition) {
+    // block of statements executed if condition is true
+    statement 1;
+    statement 2;
+    statement 3;
+}
---
+++ Second Half Chapter 1/If_Else_conditions.js
@@ -0,0 +1,14 @@
+if (CSSConditionRule) statement 1;
+
+else statement 2;
+
+if (CSSConditionRule) {
+    statement 1;
+    statement 2;
+    statement 3;
+}
+else {
+    statement 5;
+    statement 6;
+    statement 7;
+}
